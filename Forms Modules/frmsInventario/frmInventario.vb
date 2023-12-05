d0d66f95916afc91873eab9623cd86f969a54bfa
aaron09aa <aaronalvarado2509@gmail.com>
2023-12-04 23:40:47 -0500

    a

--- Forms Modules/frmsInventario/frmInventario.vb
+++ Forms Modules/frmsInventario/frmInventario.vb
@@ -1,9 +1,57 @@
-﻿Public Class frmInventario
-    Private Sub Inventario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Imports MySql.Data.MySqlClient
+
+Public Class frmInventario
+
+    Dim invenatyDao As New inventaryDAO(myConnectionDB)
+
+    Private Sub frmInventario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            conexionDB()
+            myConnectionDB.Open()
+            Dim InventarioDataTable As DataTable = invenatyDao.LlenarDGVInventario
+            dgvInv.DataSource = InventarioDataTable
+        Catch ex As Exception
+            MsgBox("Error al realizar la conexión" & vbCrLf & "Error: " & ex.Message, MsgBoxStyle.Critical, "Error")
+        Finally
+            If myConnectionDB.State <> ConnectionState.Closed Then myConnectionDB.Close()
+        End Try
+    End Sub
+
+    Private Sub tsMarcaInv_Click(sender As Object, e As EventArgs)
+        '  SetPanel(frmAgregarMarca, panelFrmInventario)
+    End Sub
+
+    Private Sub tsProductoInv_Click(sender As Object, e As EventArgs) Handles tsProductoInv.Click
+        '   SetPanel(frmAgregarProducto, panelFrmInventario)
+    End Sub
+
+    Private Sub tsCategoriaInv_Click(sender As Object, e As EventArgs) Handles tsCategoriaInv.Click
+        Dim userInput As String
+        userInput = InputBox("Introduzca el nombre de la categoria que desee agregar:", "Agregar Categoria")
+    End Sub
+
+    Private Sub tsRealizarCompraInv_Click(sender As Object, e As EventArgs) Handles tsRealizarCompraInv.Click
 
     End Sub
 
-    Private Sub dgvInv_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvInv.CellContentClick
+    'Private Sub tsMarcaEnt_Click(sender As Object, e As EventArgs)
+    '    SetPanel(frmAgregarMarca, panelFrmInventario)
+    'End Sub
+
+    'Private Sub tsProductoEnt_Click(sender As Object, e As EventArgs) Handles tsProductoEnt.Click
+    '    SetPanel(frmAgregarProducto, panelFrmInventario)
+    'End Sub
+
+    Private Sub tsCategoriaEnt_Click(sender As Object, e As EventArgs) Handles tsCategoriaEnt.Click
+        Dim userInput As String
+        userInput = InputBox("Introduzca el nombre de la categoria que desee agregar:", "Agregar Categoria")
+    End Sub
+
+    Private Sub tsRealizarCompraEnt_Click(sender As Object, e As EventArgs) Handles tsRealizarCompraEnt.Click
+
+    End Sub
+
+    Private Sub tsAgregarInv_Click(sender As Object, e As EventArgs)
 
     End Sub
 End Class
