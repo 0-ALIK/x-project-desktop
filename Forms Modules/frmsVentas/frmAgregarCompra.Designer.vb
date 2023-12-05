38c38268e17ec975bb94bfcf3e6b69fa342d548d
aaron09aa <aaronalvarado2509@gmail.com>
2023-12-04 20:15:42 -0500

    procedimiento almacenado y nuevo formulario

---
+++ Forms Modules/frmsVentas/frmAgregarCompra.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAgregarCompra
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.panelFrmInventario = New System.Windows.Forms.Panel()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.MySqlCommand1 = New MySqlConnector.MySqlCommand()
+        Me.cboClientes = New System.Windows.Forms.ComboBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.dtpFechaC = New System.Windows.Forms.DateTimePicker()
+        Me.IconButton1 = New FontAwesome.Sharp.IconButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.panelFrmInventario.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'panelFrmInventario
+        '
+        Me.panelFrmInventario.BackColor = System.Drawing.Color.FromArgb(CType(CType(14, Byte), Integer), CType(CType(19, Byte), Integer), CType(CType(21, Byte), Integer))
+        Me.panelFrmInventario.Controls.Add(Me.Panel1)
+        Me.panelFrmInventario.Controls.Add(Me.Label11)
+        Me.panelFrmInventario.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.panelFrmInventario.Location = New System.Drawing.Point(0, 0)
+        Me.panelFrmInventario.Margin = New System.Windows.Forms.Padding(75, 77, 75, 77)
+        Me.panelFrmInventario.Name = "panelFrmInventario"
+        Me.panelFrmInventario.Padding = New System.Windows.Forms.Padding(20)
+        Me.panelFrmInventario.Size = New System.Drawing.Size(545, 680)
+        Me.panelFrmInventario.TabIndex = 19
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Arial", 14.0!)
+        Me.Label11.ForeColor = System.Drawing.Color.White
+        Me.Label11.Location = New System.Drawing.Point(183, 38)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(147, 22)
+        Me.Label11.TabIndex = 53
+        Me.Label11.Text = "Realizar compra"
+        '
+        'Panel5
+        '
+        Me.Panel5.BackColor = System.Drawing.Color.FromArgb(CType(CType(22, Byte), Integer), CType(CType(29, Byte), Integer), CType(CType(33, Byte), Integer))
+        Me.Panel5.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel5.Location = New System.Drawing.Point(0, 0)
+        Me.Panel5.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Padding = New System.Windows.Forms.Padding(15, 0, 15, 15)
+        Me.Panel5.Size = New System.Drawing.Size(545, 680)
+        Me.Panel5.TabIndex = 16
+        '
+        'MySqlCommand1
+        '
+        Me.MySqlCommand1.CommandTimeout = 0
+        Me.MySqlCommand1.Connection = Nothing
+        Me.MySqlCommand1.Transaction = Nothing
+        Me.MySqlCommand1.UpdatedRowSource = System.Data.UpdateRowSource.None
+        '
+        'cboClientes
+        '
+        Me.cboClientes.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboClientes.FormattingEnabled = True
+        Me.cboClientes.Location = New System.Drawing.Point(16, 332)
+        Me.cboClientes.Margin = New System.Windows.Forms.Padding(2)
+        Me.cboClientes.Name = "cboClientes"
+        Me.cboClientes.Size = New System.Drawing.Size(387, 38)
+        Me.cboClientes.TabIndex = 20
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.IconButton1)
+        Me.Panel1.Controls.Add(Me.dtpFechaC)
+        Me.Panel1.Controls.Add(Me.cboClientes)
+        Me.Panel1.Location = New System.Drawing.Point(62, 63)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(421, 488)
+        Me.Panel1.TabIndex = 53
+        '
+        'dtpFechaC
+        '
+        Me.dtpFechaC.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtpFechaC.Location = New System.Drawing.Point(16, 46)
+        Me.dtpFechaC.Margin = New System.Windows.Forms.Padding(2)
+        Me.dtpFechaC.Name = "dtpFechaC"
+        Me.dtpFechaC.Size = New System.Drawing.Size(387, 32)
+        Me.dtpFechaC.TabIndex = 58
+        '
+        'IconButton1
+        '
+        Me.IconButton1.BackColor = System.Drawing.Color.FromArgb(CType(CType(158, Byte), Integer), CType(CType(173, Byte), Integer), CType(CType(230, Byte), Integer))
+        Me.IconButton1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(158, Byte), Integer), CType(CType(173, Byte), Integer), CType(CType(230, Byte), Integer))
+        Me.IconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(22, Byte), Integer), CType(CType(29, Byte), Integer), CType(CType(33, Byte), Integer))
+        Me.IconButton1.IconChar = FontAwesome.Sharp.IconChar.Upload
+        Me.IconButton1.IconColor = System.Drawing.Color.FromArgb(CType(CType(22, Byte), Integer), CType(CType(29, Byte), Integer), CType(CType(33, Byte), Integer))
+        Me.IconButton1.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton1.IconSize = 35
+        Me.IconButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton1.Location = New System.Drawing.Point(110, 420)
+        Me.IconButton1.Name = "IconButton1"
+        Me.IconButton1.Size = New System.Drawing.Size(167, 44)
+        Me.IconButton1.TabIndex = 59
+        Me.IconButton1.Text = "Agregar"
+        Me.IconButton1.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 14.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(162, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 22)
+        Me.Label1.TabIndex = 60
+        Me.Label1.Text = "Fecha"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(22, Byte), Integer), CType(CType(29, Byte), Integer), CType(CType(33, Byte), Integer))
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(16, 184)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(387, 32)
+        Me.TextBox1.TabIndex = 62
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 14.0!)
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(152, 159)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 22)
+        Me.Label2.TabIndex = 61
+        Me.Label2.Text = "Cantidad"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 14.0!)
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(152, 308)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(87, 22)
+        Me.Label3.TabIndex = 63
+        Me.Label3.Text = "Producto"
+        '
+        'frmAgregarCompra
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(545, 680)
+        Me.Controls.Add(Me.panelFrmInventario)
+        Me.Controls.Add(Me.Panel5)
+        Me.Name = "frmAgregarCompra"
+        Me.Text = "frmAgregarCompra"
+        Me.panelFrmInventario.ResumeLayout(False)
+        Me.panelFrmInventario.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents panelFrmInventario As Panel
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents IconButton1 As FontAwesome.Sharp.IconButton
+    Friend WithEvents dtpFechaC As DateTimePicker
+    Friend WithEvents cboClientes As ComboBox
+    Friend WithEvents MySqlCommand1 As MySqlConnector.MySqlCommand
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Forms Modules/frmsVentas/frmAgregarCompra.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmAgregarCompra
+
+End Class
