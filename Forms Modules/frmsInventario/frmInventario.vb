2b17e83bfd65f9bf7e78e04716562891b688343d
aaron09aa <aaronalvarado2509@gmail.com>
2023-12-04 23:39:01 -0500

    prueba de realizar compra

--- Forms Modules/frmsInventario/frmInventario.vb
+++ Forms Modules/frmsInventario/frmInventario.vb
@@ -2,4 +2,8 @@
     Private Sub Inventario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub dgvInv_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvInv.CellContentClick
+
+    End Sub
 End Class
