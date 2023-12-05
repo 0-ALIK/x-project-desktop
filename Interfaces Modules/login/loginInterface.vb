5d9c7e005f06e3115b0a0100a64b1063a9c20c66
COviedoD <135085372+COviedoD@users.noreply.github.com>
2023-12-05 16:17:50 -0500

    Login
    Creación de las frms y la clase DAO respectivamente. Creación del procedimiento almacenado.

---
+++ Interfaces Modules/login/loginInterface.vb
@@ -0,0 +1,5 @@
+﻿Public Interface loginInterface
+    Function InsertarUsuario(ByVal nombre As String, ByVal correo As String, ByVal pass As String, ByVal rol As String) As Integer
+    Function EncriptarPass(pass As String) As Integer
+
+End Interface
