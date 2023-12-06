302b62c10ec084d88df4b312ebf2d0388dad3f4a
COviedoD <135085372+COviedoD@users.noreply.github.com>
2023-12-05 19:42:10 -0500

    Encriptación

--- Class Modules/login/loginDAO.vb
+++ Class Modules/login/loginDAO.vb
@@ -1,4 +1,7 @@
-﻿Imports System.Data.SqlClient
+﻿Imports System.Configuration
+Imports System.Data.SqlClient
+Imports System.Security.Cryptography
+Imports System.Text
 Imports MySql.Data.MySqlClient
 Public Class loginDAO
     Implements loginInterface
@@ -10,12 +13,14 @@ Public Class loginDAO
     Public Function InsertarUsuario(nombre As String, correo As String, pass As String, rol As String) As Integer Implements loginInterface.InsertarUsuario
         Dim resultado As Integer = 0
         Try
+            Dim passEncriptada As String = EncriptarPass(pass)
+
             Using glCommand As New SqlCommand("SP_IngresarUser", myConnectionDB)
                 glCommand.CommandTimeout = 0
                 glCommand.CommandType = CommandType.StoredProcedure
                 glCommand.Parameters.AddWithValue("@p_nombre", nombre)
                 glCommand.Parameters.AddWithValue("@p_correo", correo)
-                glCommand.Parameters.AddWithValue("@p_pass", pass)
+                glCommand.Parameters.AddWithValue("@p_pass", passEncriptada)
                 glCommand.Parameters.AddWithValue("@p_rol", rol)
                 myConnectionDB.Open()
                 resultado = glCommand.ExecuteNonQuery()
@@ -29,6 +34,20 @@ Public Class loginDAO
     End Function
 
     Public Function EncriptarPass(pass As String) As Integer Implements loginInterface.EncriptarPass
-        Throw New NotImplementedException()
+        Dim key As String = ConfigurationManager.AppSettings("ClaveTripleDES")
+        Dim iv As String = "qualityi"
+
+        Using des As New TripleDESCryptoServiceProvider()
+            des.Key = Encoding.UTF8.GetBytes(key)
+            des.IV = Encoding.UTF8.GetBytes(iv)
+
+            Using transform As ICryptoTransform = des.CreateEncryptor()
+                Dim buffer() As Byte = Encoding.UTF8.GetBytes(pass)
+                Dim encryptedBytes() As Byte = transform.TransformFinalBlock(buffer, 0, buffer.Length)
+                Return Convert.ToBase64String(encryptedBytes)
+            End Using
+
+        End Using
+
     End Function
 End Class
